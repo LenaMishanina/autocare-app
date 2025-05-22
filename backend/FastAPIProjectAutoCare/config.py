b8802101224467f90ecc6d9646d323a31e4466aa
Вагина Дарья <dvagina@inbox.ru>
2025-05-23 00:49:10 +0300

    add files calendar and history

--- backend/FastAPIProjectAutoCare/config.py
+++ backend/FastAPIProjectAutoCare/config.py
@@ -8,6 +8,6 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 APP_WNV = os.getenv('APP_WNV', 'development')
 DATABASE_USERNAME = os.getenv('DATABASE_USERNAME', 'postgres')
-DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', '!234Qwer')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'Fa4bixe')
 DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost')
 DATABASE_NAME = os.getenv('DATABASE_NAME', 'AutoCare')
