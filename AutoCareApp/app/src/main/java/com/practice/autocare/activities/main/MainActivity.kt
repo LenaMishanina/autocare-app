f18dbd79277a0b01b99a5e59f7e41b9c674c980c
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2025-05-23 17:41:53 +0300

    calendar (logic and improve design)

--- AutoCareApp/app/src/main/java/com/practice/autocare/activities/main/MainActivity.kt
+++ AutoCareApp/app/src/main/java/com/practice/autocare/activities/main/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.practice.autocare.activities.main
 
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 import com.practice.autocare.R
 import com.practice.autocare.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,9 +19,25 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        чтобы избежать дублирования при повороте экрана
+
+        setSupportActionBar(binding.mainToolbar)
+        // todo setSupportActionBar?.title = машина пользователя (марка и модель)
+
+        binding.navView.setNavigationItemSelectedListener(this)
+        val toggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            binding.mainToolbar,
+            R.string.open_nav,
+            R.string.close_nav,
+        )
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        // чтобы избежать дублирования при повороте экрана
         if (savedInstanceState == null) {
             replaceFragment(CalendarFragment())
+            binding.navView.setCheckedItem(R.id.nav_calendar)
         }
 
         binding.bottomNavigationView.setOnItemSelectedListener {
@@ -37,5 +57,15 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.replace(R.id.mainFrameLayout,fragment)
         fragmentTransaction.commit()
 
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_accSettings -> Toast.makeText(this, "посмотреть аккаунт", Toast.LENGTH_SHORT).show()
+            R.id.nav_settings -> Toast.makeText(this, "посмотреть настройки", Toast.LENGTH_SHORT).show()
+            R.id.nav_exit -> Toast.makeText(this, "выйти из аккаунта", Toast.LENGTH_SHORT).show()
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
