a5f5bf737c4957e0f01f9be7eb13e7b7f7247a79
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2025-05-24 08:25:30 +0300

    auto activate new service field

--- AutoCareApp/app/src/main/java/com/practice/autocare/activities/main/CalendarFragment.kt
+++ AutoCareApp/app/src/main/java/com/practice/autocare/activities/main/CalendarFragment.kt
@@ -1,12 +1,14 @@
 package com.practice.autocare.activities.main
 
 import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
@@ -222,6 +224,7 @@ class CalendarFragment : Fragment() {
                 dialog.dismiss()
             }
             setNegativeButton("Отмена") { dialog, _ -> dialog.cancel() }
+
             show()
         }
     }
@@ -244,6 +247,7 @@ class CalendarFragment : Fragment() {
             )
             setText(initialValue)
             hint = "Введите услугу"
+            requestFocus()// Автоматически запрашиваем фокус
         }
         // Удалить услугу при добавлении
         val removeButton = ImageButton(context).apply {
@@ -257,6 +261,14 @@ class CalendarFragment : Fragment() {
                 container.removeView(inputLayout)
             }
         }
+
+        // Показываем клавиатуру для нового поля
+        editText.post {
+            editText.requestFocus()
+            val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT)
+        }
+
         inputLayout.addView(editText)
         inputLayout.addView(removeButton)
         container.addView(inputLayout) //Поле ввода услуги
